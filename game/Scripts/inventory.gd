6339929311788390afc223924087446bf557033c
Brooke Feely - Laptop <brookefeely@gmail.com>
2025-01-25 15:04:41 +1100

    Chippin away - some minor inventory bits (reference player, initial setup_

--- game/Scripts/inventory.gd
+++ game/Scripts/inventory.gd
@@ -1,11 +1,25 @@
 extends Node
 
+#Prevent needing to reference elsewhere
+var player_Node:Node = null
+var WordlistScript:Script
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#Inventory Array (didn't make resizable)
+var inventory = []
 
+#Possibly use later to easily work out which types will be in which slots?
+#Eg Maybe max hold 2 nouns, 2 adjs, 2, verbs
+#@export var word_type
+#@export var word_effect
+#var scenepath:String = "res://Scenes/inventory.tscn"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _addWord():
 	pass
+
+func _removeWord():
+	pass
+
+#Add a reference into the player script to set the ref:
+#inventory.SetPlayerReference.self
+func _setPlayerReference(player):
+	player_Node = player
