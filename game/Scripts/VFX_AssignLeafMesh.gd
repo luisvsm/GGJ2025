f0ef0963330a3a571787fac049dd1000d0b8d55e
Brooke Feely - Laptop <brookefeely@gmail.com>
2025-01-26 10:56:25 +1100

    VFX script testing - currently plays in scene and will be destroyed after 4 seconds, but isn't choosing meshes

---
+++ game/Scripts/VFX_AssignLeafMesh.gd
@@ -0,0 +1,19 @@
+extends GPUParticles3D
+
+@export var mesh1:Mesh 
+@export var mesh2:Mesh 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	UpdateMeshVariables()
+	await get_tree().create_timer(4.0).timeout
+	queue_free()
+
+func SetMeshes(mesh1Input:Mesh, mesh2Input:Mesh) -> void:
+	mesh1 = mesh1Input
+	mesh2 = mesh2Input
+	UpdateMeshVariables()
+
+func UpdateMeshVariables():
+	draw_pass_1 = mesh1
+	draw_pass_2 = mesh2
