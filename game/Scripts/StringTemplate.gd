7d7c47ebbe92411de5a9b20d4553df23b1c82e6f
Luis Van Slageren <luisvsm@gmail.com>
2025-01-25 22:47:29 +1100

    added function for hiding player inputs for displaying in player facing areas (adding ____ instead of [noun] etc) Added spot for string templates

--- game/Scripts/StringTemplate.gd
+++ game/Scripts/StringTemplate.gd
@@ -1,11 +1,33 @@
 extends Node
 
+static var TemplateList:Array[String] = [
+	"Look for the [object descriptor] [object], especially when it’s cloudy."
+]
+
+static func HidePlayerBrackets(input:String, bracketOpen:String = "[", bracketClose:String = "]")->String:
+	var openingBracket = input.find(bracketOpen)
+	var closingBracket = input.find(bracketClose)
+	if openingBracket >= 0 && closingBracket >= 0:
+		var stringToReplace = input.substr(openingBracket+1, closingBracket-openingBracket-1)
+		return "%s%s%s"%[
+			input.substr(0, openingBracket),
+			"____",
+			input.substr(closingBracket+1)
+		]
+		
+	return input
+	
 static func Process(input:String, bracketOpen:String = "{", bracketClose:String = "}")->String:
 	var openingBracket = input.find(bracketOpen)
 	var closingBracket = input.find(bracketClose)
 	if openingBracket >= 0 && closingBracket >= 0:
 		var stringToReplace = input.substr(openingBracket+1, closingBracket-openingBracket-1)
-		return Process(input.substr(0, openingBracket) + TagToWord(stringToReplace) + input.substr(closingBracket+1)) 
+		return Process("%s%s%s"%[
+			input.substr(0, openingBracket),
+			TagToWord(stringToReplace),
+			input.substr(closingBracket+1)
+			]
+		) 
 		
 	return input
 	
