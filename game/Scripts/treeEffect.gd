b848d4dc8248a168f609b71d7896d2c2983cbd57
Luis Van Slageren <luisvsm@gmail.com>
2025-01-25 21:11:30 +1100

    Updated leaf colours & meshes

--- game/Scripts/treeEffect.gd
+++ game/Scripts/treeEffect.gd
@@ -20,25 +20,20 @@ static func create(Inputtype:EffectType,Inputvalue) -> TreeEffect:
 	return newInstanceofTreeEffect
 
 enum LeafColours{
-	Red,
-	Yellow,
-	Green,
-	Blue,
-	Pink,
-	Orange,
-	White,
-	Purple,
-	Black,
-	Indigo,
-	Violet,
-	Emerald,
-	Ruby,
-	Taupe,
-	Cream,
-	Topaz,
-	Aqua,
-	Silver,
-	Copper,
+	TeaGreen,
+	ForestGreen,
+	SkyBlue,
+	OlympicBlue,
+	CandyRed,
+	CurrantRed,
+	PineappleYellowBlending,
+	Dijon,
+	CadmiumWhite,
+	Grey,
+	SlateGrey,
+	Coal,
+	TigerOrange,
+	Bronze,
 }
 
 enum LeafPattern{
@@ -50,4 +45,63 @@ enum LeafPattern{
 enum LeafShape{
 	Maple,
 	Fig
+}
+
+var LeafMeshByColourAndType:Dictionary = {
+	LeafColours.TeaGreen:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.ForestGreen:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.SkyBlue:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.OlympicBlue:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.CandyRed:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.CurrantRed:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.PineappleYellowBlending:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.Dijon:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.CadmiumWhite:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.Grey:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.SlateGrey:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.Coal:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.TigerOrange:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
+	LeafColours.Bronze:{
+		LeafShape.Maple:preload("res://art_assets/Leaf_Test_WithColour.obj"),
+		LeafShape.Fig:preload("res://art_assets/Leaf_Test_WithColour.obj")
+	},
 }
