ce852b5506be3bb4e1f1573fe33c44ef0047e773
Sally-Williams <anovelideabysally@gmail.com>
2025-01-26 12:19:42 +1100

    Added new audio tracks and sound strings

--- game/Scripts/AudioManager.gd
+++ game/Scripts/AudioManager.gd
@@ -9,7 +9,27 @@ var music:Dictionary
 
 func _ready() -> void:
 	sounds = {
-		#"test":preload("res://Audio/test.mp3")
+		"baby1":preload("res://Audio/Baby01.wav"),
+		"baby2":preload("res://Audio/Baby02.wav"),
+		"baby3":preload("res://Audio/Baby03.wav"),
+		"flap1":preload("res://Audio/Flap01.wav"),
+		"flap2":preload("res://Audio/Flap02.wav"),
+		"flap3":preload("res://Audio/Flap03.wav"),
+		"flap4":preload("res://Audio/Flap04.wav"),
+		"newbranch1":preload("res://Audio/NewBranch01.wav"),
+		"newbranch2":preload("res://Audio/NewBranch02.wav"),
+		"pullworm1":preload("res://Audio/PullWorm01.wav"),
+		"pullworm2":preload("res://Audio/PullWorm02.wav"),
+		"success-convert1":preload("res://Audio/SuccessConvertToBush_Rustle01.wav"),
+		"success-convert2":preload("res://Audio/SuccessConvertToBush_Rustle02.wav"),
+		"success-convert3":preload("res://Audio/SuccessConvertToBush_Rustle03.wav"),
+		"success-sentence":preload("res://Audio/SuccessSentence.wav"),
+		"success-word1":preload("res://Audio/SuccessWord_Chirp01.wav"),
+		"success-word2":preload("res://Audio/SuccessWord_Chirp02.wav"),
+		"success-word3":preload("res://Audio/SuccessWord_Chirp03.wav"),
+		"worm1":preload("res://Audio/WormSpawns01.wav"),
+		"worm2":preload("res://Audio/WormSpawns02.wav"),
+		"worm3":preload("res://Audio/WormSpawns03.wav"),
 	}
 	music = {
 		#"test":preload("res://Audio/test.mp3")
